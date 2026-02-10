12985c03e36dd24c3ba0f777a28faa3fab4a70c0
ryan-pelchat <gitryanpelchat@gmail.com>
2026-02-10 15:44:37 -0500

    Update timeDiffCalculator.py
    - just changed default time

--- Documentation/ScriptsToAidDocumentation/timeDiffCalculator.py
+++ Documentation/ScriptsToAidDocumentation/timeDiffCalculator.py
@@ -33,5 +33,4 @@ def time_diff_hhmmss(t1: str, t2: str) -> str:
     return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
 
 
-print(time_diff_hhmmss("10:10:00", "13:00:00"))
-print(time_diff_hhmmss("23:59:50", "00:00:10"))
+print(time_diff_hhmmss("14:25:00", "15:50:00"))
