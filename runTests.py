fac71c4d2b1d9c97109c6a13c6877928b245a8a0
ryan-pelchat <gitryanpelchat@gmail.com>
2026-03-06 10:05:57 -0500

    test: Added tests for video cropper function

---
+++ runTests.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+
+testsToRun = [
+    "test.logic.test_videoEditing",
+]
+
+
+def main():
+    for test in testsToRun:
+        print(f"\n\n\nTESTING THE {test} CLASS OF TESTS!!!!!!")
+        subprocess.run(
+            [
+                "python",
+                "-m",
+                test,
+            ],
+            check=True,
+        )
+
+
+if __name__ == "__main__":
+    main()
