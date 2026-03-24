1d3c269dc1934cf8845872d084ef575ffe4ab7c2
smavka <smavka@my.yorku.ca>
2026-03-24 11:20:11 -0400

    aggregated data by hour

--- src/customer-x-sales.py
+++ src/customer-x-sales.py
@@ -1,5 +1,23 @@
-import matplotlib.pyplot as plt
 from collections import defaultdict
+
+def customer_conversion_function(customers, checkouts):
+    #Returns structured data
+
+    customers_per_hour = defaultdict(int)
+    checkouts_per_hour = defaultdict(int)
+    sales_per_hour = defaultdict(float)
+
+    #Aggregate data by hour
+    #count customers
+    for c in customers:
+        hour = c.entered_at.hour
+        customers_per_hour[hour] += 1
+
+    #count checkouts and sales
+    for co in checkouts:
+        hour = co.created_at.hour
+        checkouts_per_hour[hour] += 1
+        sales_per_hour[hour] += co.total_price
 
 
 
