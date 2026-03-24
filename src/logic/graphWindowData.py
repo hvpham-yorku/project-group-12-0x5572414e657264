d929ad2f6869b8f7103840d0994f96cebe77e023
ryan-pelchat <gitryanpelchat@gmail.com>
2026-03-24 15:57:17 -0400

    Update graphWindowData.py
    feat+: boilerplate for graph window state

--- src/logic/graphWindowData.py
+++ src/logic/graphWindowData.py
@@ -10,7 +10,18 @@ class GraphWindow:
     def __init__(self):
         self._countValues = [15, 30, 45, 10]
         self._categoriesValues = ["Apples", "Bananas", "Cherries", "Dates"]
-        self._chartType = ""
+        self._chartType = "NONE"
+        self._categorySelected = "NONE"
+        self._countsSelected = []
 
     def get_graphPieData(self) -> List[int, str]:
         return [self._countValues, self._categoriesValues]
+
+    def set_chartType(self, chartType: str) -> None:
+        self._chartType = chartType
+
+    def set_categorySelected(self, categorySelected: str) -> None:
+        self._categorySelected = categorySelected
+
+    def set_countsSelected(self, countsSelected: List[str]) -> None:
+        self._countsSelected = countsSelected
