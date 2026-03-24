c9d71d4325c75892dc32688629d7619213f0270a
smavka <smavka@my.yorku.ca>
2026-03-24 16:46:44 -0400

    defined productxquantity function

---
+++ src/product_x_quantity.py
@@ -0,0 +1,23 @@
+from typing import List, Dict
+from collections import defaultdict
+
+import pylab as p
+
+from src.database.models import Purchase, Product
+
+def product_x_quantity_function(
+        purchases: List[Purchase],
+        products: List[Product]
+) -> Dict[str, int]:
+
+    #map product_id -> prod_name
+    product_names: Dict[int, str] = {p.product_id: p.name for p in products}
+
+    #Aggregate quantities
+    total_quantities: defaultdict[str, int] = defaultdict(int)
+
+    for purchase in purchases:
+        product_name = product_names.get(purchase.product_id, f"Unknown-{purchase.product_id}")
+        total_quantities[product_name] += purchase.quantity
+
+    return dict(total_quantities)
